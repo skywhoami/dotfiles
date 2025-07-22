a71d698b9943c7af2f04bd97c4cd5467c7dc9963
Skylar <hi@skylar.sh>
2025-07-22 12:24:18 +0100

    update cord.nvim stuff

--- .config/nvim/lua/plugins/discord.lua
+++ .config/nvim/lua/plugins/discord.lua
@@ -3,12 +3,17 @@ return {
   build = ":Cord update",
   opts = {
     editor = {
+      tooltip = "Neovim",
       icon = "https://raw.githubusercontent.com/IogaMaster/neovim/main/.github/assets/nixvim-dark.webp",
     },
     display = {
       theme = "catppuccin",
       flavor = "accent",
-      swap_icons = true,
     },
+    text = {
+      file_browser = true,
+      workspace = "Working on ${workspace}",
+    },
+    variables = true,
   },
 }
