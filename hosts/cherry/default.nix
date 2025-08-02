5fee21553878991aaf3b459b7713e1fdcab9b61b
Skylar <hi@skylar.sh>
2025-08-02 23:22:04 +0100

    plsss

--- hosts/cherry/default.nix
+++ hosts/cherry/default.nix
@@ -9,4 +9,11 @@
       device = "/dev/sda";
     };
   };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/f3d6d4c6-81f5-4b28-9f2e-a2a7fd98596b";
+      fsType = "ext4";
+    };
+  };
 }
