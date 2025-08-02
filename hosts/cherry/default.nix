32a9c6428bd9178fd4457e3b66ccac996ee73fde
Skylar <hi@skylar.sh>
2025-08-02 23:49:42 +0100

    just stop being a bitch

--- hosts/cherry/default.nix
+++ hosts/cherry/default.nix
@@ -1,4 +1,3 @@
-{ lib, ... }:
 {
   profiles.headless.enable = true;
 
@@ -20,52 +19,6 @@
   users.users.sky.openssh.authorizedKeys.keys = [
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG56OdAD9r6wWd+ag1R+neANX1KSdpl/h8JkYVCVdNRi''
   ];
-
-  networking = {
-    defaultGateway = "172.31.1.1";
-    defaultGateway6 = {
-      address = "fe80::1";
-      interface = "eth0";
-    };
-    dhcpcd.enable = false;
-    usePredictableInterfaceNames = lib.mkForce false;
-    interfaces = {
-      eth0 = {
-        ipv4.addresses = [
-          {
-            address = "91.99.239.117";
-            prefixLength = 32;
-          }
-        ];
-        ipv6.addresses = [
-          {
-            address = "2a01:4f8:c0c:9289::1";
-            prefixLength = 64;
-          }
-          {
-            address = "fe80::9000:6ff:fe4f:4b39";
-            prefixLength = 64;
-          }
-        ];
-        ipv4.routes = [
-          {
-            address = "172.31.1.1";
-            prefixLength = 32;
-          }
-        ];
-        ipv6.routes = [
-          {
-            address = "fe80::1";
-            prefixLength = 128;
-          }
-        ];
-      };
-
-    };
-  };
-  services.udev.extraRules = ''
-    ATTR{address}=="92:00:06:4f:4b:39", NAME="eth0"
-  '';
 
   system.stateVersion = "23.11";
 }
