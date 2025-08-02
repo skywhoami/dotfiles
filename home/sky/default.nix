6fc39e5cd5b26b70342212a4802eed1c28791e15
Skylar <hi@skylar.sh>
2025-08-02 23:18:43 +0100

    knjgdsbjgdabjhgds

--- home/sky/default.nix
+++ home/sky/default.nix
@@ -1,6 +1,7 @@
+{_class,...}:
 {
   home.username = "sky";
-  home.homeDirectory = "/Users/sky";
+  home.homeDirectory = if _class == "darwin" then "/Users/sky" else "/home/sky";
   home.stateVersion = "25.05";
   home.file.".hushlogin".text = "";
 
