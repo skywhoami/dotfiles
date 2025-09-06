c45dcb110a31b8c4dc0818ad136fbe46494ef0af
Sky <hi@luvsick.gg>
2025-09-06 17:07:27 +0100

    flake: fix llvm not building on darwin

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "sky's dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
       type = "github";
